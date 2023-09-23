d4ea7a8fb7b76896865f7f5ba797a6b0a0feeb87
Lê Thanh Phát <ltphatt2709@gmail.com>
2023-09-23 15:42:03 +0700

    Project Files

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,20 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private bool isPlayer;
     [SerializeField] private int health = 5;
     [SerializeField] private int score = 10;
+    [SerializeField] private float hurtForce = 2.5f;
     ScoreKeeper scoreKeeper;
     Player player;
+
+    Animator anim;
 
     void Awake()
     {
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
         player = FindObjectOfType<Player>();
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
     }
 
     public int GetHealth()
@@ -35,8 +43,9 @@ public class Health : MonoBehaviour
     {
         if (!isPlayer)
         {
+            anim.SetTrigger("isDead");
             scoreKeeper.ModifyScore(score);
-            Destroy(gameObject);
+            StartCoroutine(EnemyDead());
         }
         else
         {
@@ -44,16 +53,27 @@ public class Health : MonoBehaviour
         }
     }
 
+    IEnumerator EnemyDead()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
     // Attacking enemy via jumping
     void OnCollisionEnter2D(Collision2D other)
     {
+        // If this game object is enemy
         if (!isPlayer)
         {
-            if ((other.gameObject.tag != "Ground") || (other.gameObject.tag == "Player" && player.isFalling == true))
+            if (other.gameObject.tag == "Player")
             {
-                TakeDamage();
+                if (player.isFalling)
+                {
+                    TakeDamage();
+                }
             }
         }
+        // If this game object is player
         else
         {
             if (other.gameObject.tag == "Enemy")
@@ -64,6 +84,20 @@ public class Health : MonoBehaviour
                 }
                 else
                 {
+                    float posX = player.transform.position.x;
+                    float posY = player.transform.position.y;
+
+                    // Player is on the left of the enemy
+                    if (player.rb.velocity.x < other.gameObject.transform.position.x)
+                    {
+                        player.transform.position = new Vector2(posX - hurtForce, posY);
+                    }
+                    // Player is on the right of the enemy
+                    else if (player.rb.velocity.x >= other.gameObject.transform.position.x)
+                    {
+                        player.transform.position = new Vector2(posX + hurtForce, posY);
+                    }
+
                     TakeDamage();
                 }
             }
