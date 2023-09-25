89eff0bc60fe8adb799832a29a3c5e12f5e8412f
Lê Thanh Phát <ltphatt2709@gmail.com>
2023-09-25 11:15:40 +0700

    Project Files

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,16 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
         Time.timeScale = 1f;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 
